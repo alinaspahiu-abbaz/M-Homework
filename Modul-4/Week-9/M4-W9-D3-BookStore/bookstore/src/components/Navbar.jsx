8fd5fb8d557756d9d75d025919187632d19ad204
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-03 16:52:43 +0200

    Create Navbar.jsx

---
+++ Modul-4/Week-9/M4-W9-D3-BookStore/bookstore/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+  
+import React, { Component } from "react";
+import { Navbar, Nav } from "react-bootstrap";
+
+const NavBar = (props) => {
+  return (
+    <Navbar bg="dark"  variant="dark" expand="lg">
+      <Navbar.Brand href="#home">
+        {props.title} - Books are awsome!
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">About</Nav.Link>
+          <Nav.Link href="#link">Browse</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default NavBar;
