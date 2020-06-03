7f6d52909ac0e22f064cca08fd692866c448be41
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-03 16:53:16 +0200

    Create Footer.jsx

---
+++ Modul-4/Week-9/M4-W9-D3-BookStore/bookstore/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+
+const Footer = () => {
+    return (
+        <div className="d-flex flex-column sticky-bottom" >
+          <footer className="footer bg-dark" variant="dark">
+    <div>
+      <p> MyBookStore</p>
+      <span>&copy; BookStore 2020</span>
+      <p>Best BookStore</p>
+
+    </div>
+   
+  </footer>
+</div>
+
+    )
+}
+
+export default Footer;
