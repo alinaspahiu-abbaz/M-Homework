0dcd696e546156ec6e23e07c115799a894ddf45e
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-09 17:18:55 +0200

    latestRelease finished

--- Modul-4/Week-9/M4-W9-D3-BookStore/bookstore/src/components/LatestRelease.jsx
+++ Modul-4/Week-9/M4-W9-D3-BookStore/bookstore/src/components/LatestRelease.jsx
@@ -1,44 +1,41 @@
 import React, { Component } from "react";
-import books from '../jsons/horror.json';
 import {Container, Row, Col, Card, Button} from "react-bootstrap";
 
 
+
+let books = {  horror: require("../jsons/horror.json")  };
+
 class LatestRelease extends Component {
+  state = {   books: books.horror.slice(0,6)   }
 
-    render() {
-        return (
-  <Container className='col-xs-3'>
-     <Row className='' >
-        <Col xs={3} md={4}>
-        {books.map((book) => { 
-            return (
-           <Card  style={{ width: '18rem' }}>
-            
-               
-             <Card.Img variant="top" src={book.img}/>
-                        
-            <Card.Body>
-              <Card.Title>{book.title}</Card.Title>
-                <Card.Text>
-                   Some quick example text to build on the card title and make up the bulk of
-                   the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-           
-            </Card>
-           
-             )})}
-              
-
-        </Col>
-        
-     </Row>
-  </Container>
-            
-        )
-    }
-   
-}
+render() {
+  return (
+     <Container>
+       <Row className='justify-content-center'>
+         {
+          this.state.books.map((book) => 
+           { 
+             return (
+              <Col xs={12} md={4}>
+                 <Card  style={{ width: '18rem' }}>
+                   <Card.Img variant="top" src={book.img}/>
+                   <Card.Body>
+                     <Card.Title>{book.title}</Card.Title>
+                     <Card.Text>
+                       Some quick example text to build on the card title and make up the bulk of
+                       the card's content.
+                     </Card.Text>
+                       <Button variant="primary">Go somewhere</Button>
+                   </Card.Body>
+                 </Card>
+              </Col>
+               )
+             })
+          }  
+       </Row>
+     </Container>
+          )
+        }
+      }
 
 export default LatestRelease;
