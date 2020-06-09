707db897e5b12c747c7af835ff791f1841389f5b
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-09 17:19:13 +0200

    Footer finished

--- Modul-4/Week-9/M4-W9-D3-BookStore/bookstore/src/components/Footer.jsx
+++ Modul-4/Week-9/M4-W9-D3-BookStore/bookstore/src/components/Footer.jsx
@@ -1,19 +1,35 @@
 import React, {Component} from 'react';
+import { Row, Col } from 'react-bootstrap';
+import logo from "../image/logoF.png";
+
+
+
+
 
 
 const Footer = () => {
-    return (
-        <div className="d-flex flex-column sticky-bottom" >
-          <footer className="footer bg-dark" variant="dark">
-    <div>
-      <p> MyBookStore</p>
-      <span>&copy; BookStore 2020</span>
-      <p>Best BookStore</p>
+return (
+  <div className="d-flex flex-column sticky-bottom" >
+    <footer id="foo" className="footer bg-dark text-light foo">
+      <Row>
+        <Col>
+          <img src={logo} />
+        </Col>
+       
+        <Col>
+          <p className='mt-3'>  Tirane, Albania</p>
+           <span>&copy; BookStore 2020</span>
+           
+        </Col>
 
-    </div>
-   
-  </footer>
-</div>
+        <Col>
+           <p className='mb-0 mt-3'>Facebook</p>
+           <p className='m-0'>Instagram</p>
+           <p className='m-0'> Twitter</p>
+        </Col>
+      </Row>
+    </footer>
+  </div>
 
     )
 }
