b6ef355c92c2cda5042da8dd42946048348ff3db
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-12 22:01:23 +0200

    Create App.js

---
+++ Modul-4/Week-10/BenchMark-Spotify-Reacct/spotify_reacct/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import MediaPlayer from './components/MediaPlayer'
+import Search from './components/Search';
+
+function App() {
+  return (
+    <div className="App">
+     <MediaPlayer />
+     <Search />
+    </div>
+  );
+}
+
+export default App;
