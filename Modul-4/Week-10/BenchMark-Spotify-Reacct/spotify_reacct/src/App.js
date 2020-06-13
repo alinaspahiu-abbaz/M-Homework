cfc26d7a3ebb2d7d577e8ea1baa6aa8b2a6f3daa
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-13 18:29:43 +0200

    Update App.js

--- Modul-4/Week-10/BenchMark-Spotify-Reacct/spotify_reacct/src/App.js
+++ Modul-4/Week-10/BenchMark-Spotify-Reacct/spotify_reacct/src/App.js
@@ -3,15 +3,28 @@ import React from 'react';
 import MyFooter from './components/MyFooter'
 import Search from './components/Search';
 import SpotifyNavBar from './components/SpotifyNavBar'
-import './App.css';
+import {Container, Row, Col} from 'react-bootstrap'
+import './App.css'
+
+
 
 function App() {
   return (
     <div className="App">
-      <Search />
-      <SpotifyNavBar />
+      <Container fluid> 
+        <Row>
+          <Col sm={"none"} md={2}> 
+            <SpotifyNavBar />
+          </Col>
+
+          <Col  md={10}>
+             <Search />
+          </Col>
+
+        <MyFooter />
+        </Row>
+        </Container>
       
-       <MyFooter />
 
       
      
