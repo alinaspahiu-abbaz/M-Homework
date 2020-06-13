a1e06f888fbdaf92c77b6fb0d3f3f88a1b8f9549
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-13 18:29:52 +0200

    Create stylesheets.js

---
+++ Modul-4/Week-10/BenchMark-Spotify-Reacct/spotify_reacct/src/components/stylesheets.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../myStyles.css'
+
+function stylesheets() {
+    return (
+        <div>
+           <h1> StyleSheets</h1> 
+        </div>
+    )
+}
+
+export default stylesheets
+
