92ae5337f5f41036d635ec8f8cfb41831e6837b6
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-15 23:54:14 +0200

    Create App.js

---
+++ Ushtrime/ushtrime_01/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from './Home'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
