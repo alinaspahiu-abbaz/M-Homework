558f9b9ce4109e6e7e29b466cd0cdef2d9348d82
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-16 23:10:47 +0200

    Create Home.jsx

---
+++ Modul-4/Week-9/M4-W9-D5-ImproveYourBookStore/bookstore/src/components/Home.jsx
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+class Home extends Component {
+    constructor (props) {
+        super(props);
+    }
+}
