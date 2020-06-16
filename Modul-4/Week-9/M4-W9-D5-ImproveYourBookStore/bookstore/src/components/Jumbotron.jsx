5bc08bff4c849b79dd01bf96ed57cbf37c37d5af
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-16 23:10:50 +0200

    Create Jumbotron.jsx

---
+++ Modul-4/Week-9/M4-W9-D5-ImproveYourBookStore/bookstore/src/components/Jumbotron.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import { Jumbotron, Container, Button } from "react-bootstrap";
+
+
+const JumboTron = () => {
+    return (
+        <Jumbotron fluid>
+  <Container>
+    <h1>Welcome</h1>
+    <p>
+      This is the Best Bookstore you can ever find!
+    </p>
+    <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+  </Container>
+</Jumbotron>
+
+    )
+}
+
+export default JumboTron;
