5d50d2d855b4555cf5cf2926a3b7ccf43e830f39
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-17 23:08:22 +0200

    Create WarningSign.jsx

---
+++ Modul-4/Week-9/M4-W9-D4-pairProgramming/State-Props/state_props/src/components/WarningSign.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Alert} from 'react-bootstrap';
+
+const WarningSign = () => {
+    return(
+        <Alert  variant='danger'>
+        This is a  alert—check it out!
+      </Alert>
+    )
+}
+
+export default WarningSign;
