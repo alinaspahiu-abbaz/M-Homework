3eded17fb5c6e7e71dbd4a3305287052529b5afe
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-18 23:50:57 +0200

    Create MyBadge.jsx

---
+++ Modul-4/Week-9/M4-W9-D4-pairProgramming/State-Props/state_props/src/components/MyBadge.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Badge} from 'react-bootstrap';
+
+const MyBadge = () => {
+    return (
+        <Badge variant="secondary">New</Badge>
+    )
+}
+
+export default MyBadge;
