26fce8b5e73f7038cdc600b535df98d235052431
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-24 22:19:42 +0200

    Create index.js

---
+++ Modul-5/m5-w12-d2-portfolio-homework/nodejs/src/services/users/index.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const fs = require("fs")
+const path = require("path")
+const uniqid = require("uniqid")
+
+const router = express.Router()
+
+//making a function
+const readFile = (fileName) => {
+    const buffer = fs.readFileSync(path.join(__dirname, fileName))
+    return JSON.parse(buffer.toString())
+
+}
+
+router.get("/", (req, res) => {
+    const usersDB = readFile('users.json')
+   
+    if(req.query && req.query.name) {
+        const filteredUsers = usersDB.filter((user) => user.hasOwnProperty('name') && user.name === req.query.name)
+
+        res.send(filteredUsers)
+    } else {    res.send(usersDB)   }
+
+router.get("/:id", (req, res) => {
+    req.params.id = readFile('users.json')
+    const retrivedUser = usersDB.filter(user =>user.id === req.params.id)
+    res.send(retrivedUser)
+
+})
+
+    
+
+})
+
+router.post("/", (req, res) => {
+    const usersDB = readFile("users.json")
+    const newUser = req.body
+})
+
+
+module.exports = router
