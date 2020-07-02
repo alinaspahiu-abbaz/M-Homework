1af661ecf49bf3e28220bb60349797f914c0ed41
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-02 23:22:30 +0200

    Update App.js

--- Modul-5/m5-w12-d2-portfolio-homework/react/portfolio_homework/src/App.js
+++ Modul-5/m5-w12-d2-portfolio-homework/react/portfolio_homework/src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Container, Row, Col} from "react-bootstrap"
+import HomePage from "./components/HomePage"
 
-function App() {
+class App extends React.Component {
+  render() {
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <HomePage />
+      </Row>
+    </Container>
   );
+}
 }
 
 export default App;
