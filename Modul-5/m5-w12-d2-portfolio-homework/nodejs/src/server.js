681cdaaa294483170464b5bcdf07f8411bf9febe
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-02 23:22:18 +0200

    Update server.js

--- Modul-5/m5-w12-d2-portfolio-homework/nodejs/src/server.js
+++ Modul-5/m5-w12-d2-portfolio-homework/nodejs/src/server.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const usersRoutes = require("./services/users")
+const cors=require("cors")
 
 const server = express()
+
+
+server.use(cors())
 
 server.use(express.json())
 server.use('/users', usersRoutes)
 
 //const port = process.env.PORT || 3333
 
-server.listen(3001, () => {   console.log(`Server on port: 3001`)    })
+server.listen(3030, () => {   console.log(`Server on port: 3001`)    })
