4551007e67d0b545ab1de139ea5c9d22222f9647
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-07 17:37:40 +0200

    Create server.js

---
+++ Modul-6/Week-14/m6-w14-d2-students-portfolio/nodejs/src/server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const cors = require("cors")
+const path = require("path")
+const studentsRouter = require("./students")
+const mongoose = require("mongoose")
+
+
+const server = express()
+
+server.use(express.json())
+
+server.use(cors())
+
+
+
+server.use("/students", studentsRouter)
+
+mongoose.connect("mongodb://localhost:27017/m6-w14-d2-hm-student-portofolio",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then (
+
+server.listen(3031, ()=>{
+    console.log("Find me on port 3031!")
+})
+).catch((err) => console.log(err))
