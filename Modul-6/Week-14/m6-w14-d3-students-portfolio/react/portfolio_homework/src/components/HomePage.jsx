33a9e81f8fa2e47bf00865f53a54748406038296
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-08 22:49:05 +0200

    Create HomePage.jsx

---
+++ Modul-6/Week-14/m6-w14-d3-students-portfolio/react/portfolio_homework/src/components/HomePage.jsx
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+import Students from "./Students"
+import { Accordion, Card, Col, Button, Row, Container } from "react-bootstrap"
+import SingleStudent from './SingleStudent'
+
+
+export default class HomePage extends Component {
+    state = {
+        students:[]
+    }
+
+    render() {
+        return (
+            <Container>
+                <Row>
+            
+                    {this.state.students.map(student =>
+                        <Col md={4}>
+                         <SingleStudent item={student} />
+                         </Col>
+                         )}
+                </Row>
+            </Container>
+            // <Container fluid >
+            //     <Row className="mb-5 justify-content-center">
+            //         <h1> Welcome to Students Portofolio</h1>
+            //     </Row>
+            //     <Row>
+
+            //         <Container>
+                        
+            //             <Accordion defaultActiveKey="0">
+            //                 <Card>
+            //                     <Card.Header>
+            //                         <Accordion.Toggle as={Button} variant="outline-secondary" eventKey="0">
+            //                             Click to open the List!
+            //                         </Accordion.Toggle>
+            //                     </Card.Header>
+            //                     <Accordion.Collapse eventKey="0">
+            //                         <>
+            //                         <Card.Header style={{backgroundColor:"#ab3562"}}>
+            //                               <Row style={{fontSize:"20px", fontWeight:"bold"}} className="mb-3 justify-content-center">
+            //                                 <Col sm={2}>Name</Col>
+            //                                 <Col sm={2}>Surname</Col>
+            //                                 <Col sm={2}>Email</Col>
+            //                                <Col sm={2}>Year</Col>
+            //                                <Col sm={2}>Country</Col>
+            //                                <Col sm={2}>Changes</Col>
+                                           
+            //                                </Row>
+                                             
+            //                         </Card.Header>
+            //                         <Card.Body style={{backgroundColor:"#de85a7"}}>
+            //                         <Students />
+            //                         <Row className="justify-content-center">
+            //                         <Button className="mt-4" variant="outline-dark">Add a new Student</Button>
+                                    
+                                    
+            //                         </Row>
+            //                         </Card.Body>
+            //                         </>
+            //                     </Accordion.Collapse>
+            //                 </Card>
+
+            //             </Accordion>
+            //         </Container>
+            //     </Row>
+            // </Container>
+
+        )
+    }
+    componentDidMount = async () => {
+
+        const studentsResp = await fetch("http://localhost:3032/students")
+        const students = await studentsResp.json()
+        this.setState({
+            students: students
+        })
+        console.log(students)
+    }
+
+
+}
