76c48ddd2a7911faac2229648ac7137157c91c9c
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-08 22:49:02 +0200

    Create BackOffice.jsx

---
+++ Modul-6/Week-14/m6-w14-d3-students-portfolio/react/portfolio_homework/src/components/BackOffice.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import Students from "./Students"
+import { Accordion, Card, Col, Button, Row, Container } from "react-bootstrap"
+import SingleStudentListItem from './SingleStudentListItem'
+
+
+export default class BackOffice extends Component {
+    state = {
+        students:[]
+    }
+
+    render() {
+        return (
+            <Container>
+                <Row>
+            
+                    {this.state.students.map(student =>
+                        <Col md={4}>
+                         <SingleStudentListItem item={student} />
+                         </Col>
+                         )}
+                </Row>
+            </Container>
+
+        )
+    }
+    componentDidMount = async () => {
+
+        const studentsResp = await fetch("http://localhost:3032/students")
+        const students = await studentsResp.json()
+        this.setState({
+            students: students
+        })
+        console.log(students)
+    }
+
+
+}
