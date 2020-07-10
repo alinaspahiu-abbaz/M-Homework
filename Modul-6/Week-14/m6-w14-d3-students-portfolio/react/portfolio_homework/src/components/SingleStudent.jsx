ad945f39cce3941e3a3c69d6050450d229eb104d
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-10 16:31:50 +0200

    Update SingleStudent.jsx

--- Modul-6/Week-14/m6-w14-d3-students-portfolio/react/portfolio_homework/src/components/SingleStudent.jsx
+++ Modul-6/Week-14/m6-w14-d3-students-portfolio/react/portfolio_homework/src/components/SingleStudent.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Card, Button} from "react-bootstrap"
+import {Card, Button, Row} from "react-bootstrap"
 
 export default class SingleStudent extends Component {
     
@@ -8,16 +8,24 @@ export default class SingleStudent extends Component {
         const {name, surname, email, year, country, _id, img} = this.props.item
 
         return (
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={img} />
-                <Card.Body>
-        <Card.Title>{name} - {surname}</Card.Title>
+            
+            <Row className="mt-4 d-flex align-items0center justify-content-space-between">
+                <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" style={{}} src={img} />
+                <Card.Body style={{backgroundColor:"grey"}}>
+                <Row className="justify-content-center">
+                <Card.Title >{name} {surname}</Card.Title></Row>
                     <Card.Text>
                        {email} - {year} - {country}
                 </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
+                <Row className="justify-content-center">
+                    <Button className="mr-3" variant="outline-warning">Projects</Button>
+                    <Button className="mr-3" variant="outline-info">Edit</Button>
+                    <Button variant="outline-danger">Delete</Button>
+                    </Row>
                 </Card.Body>
-            </Card>
+                </Card>
+                </Row>
         )
     }
 }
