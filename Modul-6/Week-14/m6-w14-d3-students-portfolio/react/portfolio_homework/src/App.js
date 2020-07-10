ee6b3616500aad230510e89a14ffcfe50f269171
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-10 23:56:10 +0200

    Update App.js

--- Modul-6/Week-14/m6-w14-d3-students-portfolio/react/portfolio_homework/src/App.js
+++ Modul-6/Week-14/m6-w14-d3-students-portfolio/react/portfolio_homework/src/App.js
@@ -1,31 +1,48 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import './index.css'
-import {Container, Row, Col,Pagination} from "react-bootstrap"
+import { Container, Row, Col, Pagination } from "react-bootstrap"
 import HomePage from "./components/HomePage"
 import MyNavbar from "./components/MyNavbar"
-import {BrowserRouter as Router, Link, Switch, Route} from "react-router-dom"
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom"
 
 import BackOffice from './components/BackOffice';
 
 class App extends React.Component {
- 
+
   render() {
-    
-  return (
-    <>
-   
-    <MyNavbar  />
-   
-     
-      <Container >
-    
-      <HomePage/>
-    
-      </Container>
-   </>
-  );
-}
+
+    return (
+      <>
+
+        <MyNavbar />
+
+
+        <Container >
+
+          <HomePage />
+          <Row className="mt-3 justify-content-center">
+            <Pagination>
+              <Pagination.First />
+              <Pagination.Prev />
+
+
+              <Pagination.Item active>{1}</Pagination.Item>
+              <Pagination.Item>{2}</Pagination.Item>
+              <Pagination.Item >{3}</Pagination.Item>
+
+
+
+
+              <Pagination.Next />
+              <Pagination.Last />
+            </Pagination>
+          </Row>
+
+        </Container>
+      </>
+    );
+  }
 
 }
 
