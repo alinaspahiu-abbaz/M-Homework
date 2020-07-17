42f91b86641c5e44a6f01f08d2581d911dc7aac7
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-17 16:12:43 +0200

    Create MyJumb.jsx

---
+++ Modul-6/Week-15/m5-w15-d5-benchmark/m5-w15-d4/frontend/src/components/MyJumb.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MyJumb extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
