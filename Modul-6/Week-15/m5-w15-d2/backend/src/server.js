dbeab0ae14cf0d4ce163e3a0615034941b486497
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-19 21:24:52 +0200

    Create server.js

---
+++ Modul-6/Week-15/m5-w15-d2/backend/src/server.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const cors = require("cors")
+const dotenv = require("dotenv")
+dotenv.config()
+const db = require("./db")
+const studentRouter = require("./routes/students")
+
+const server = express()
+
+server.use(cors())
+server.use(express.json())
+
+server.get("/", (req, res) => {
+res.send("The server is running")
+})
+
+server.use("/students", studentRouter)
+
+
+server.listen(3028 || 3456, ()=>
+console.log("Runing on: 3028 " ))
