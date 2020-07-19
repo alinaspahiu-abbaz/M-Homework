9cb2b1c8f756af6206081fec2c328a479a692cfd
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-19 21:25:06 +0200

    Create StudentList.jsx

---
+++ Modul-6/Week-15/m5-w15-d2/frontend/src/components/StudentList.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from "react"
+import {Container, Row} from "react-bootstrap"
+import StudentListItem from "./StudentListItem"
+
+class StudentList extends Component {
+    render (){
+        return(
+            <Container className="my-5">
+                <Row>
+                    {this.props.students.map(x => <StudentListItem key={x._id} item={x} />)}
+                </Row>
+            </Container>
+        )
+    }
+}
+
+export default StudentList
