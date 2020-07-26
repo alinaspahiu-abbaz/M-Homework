0fe44a620706ec0174d2458d1d5d55cc1ef326b2
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-26 18:29:24 +0200

    Update App.js

--- Modul-4/Week-9/M4-W9-D3-BookStore/bookstore/src/App.js
+++ Modul-4/Week-9/M4-W9-D3-BookStore/bookstore/src/App.js
@@ -3,28 +3,18 @@ import './App.css';
 import NavBar from "./components/Navbar";
 import Footer from "./components/Footer";
 import JumboTron from "./components/Jumbotron";
-import MyBooks from "./components/MyBooks";
 import LatestRelease from './components/LatestRelease'
-import {Container, Row, Col} from "react-bootstrap";
-
-
 
 
 function App()
  {
    return (
-     <>
            <div className="App">
               <NavBar title="MyBookStore" />
-              <JumboTron/>
-              
-              <LatestRelease />
-              
+                 <JumboTron/>
+                 <LatestRelease />
               <Footer />
-           </div>
-           </>
-
-           
+           </div> 
           );
  }
 
