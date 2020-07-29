1ec01cea58a0a8ed58bc71b86d27445cdf2ffdfa
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-29 21:12:37 +0200

    Update server.js

--- Modul-6/Week-15/m5-w15-d4/backend/src/server.js
+++ Modul-6/Week-15/m5-w15-d4/backend/src/server.js
@@ -2,19 +2,26 @@ const express = require("express")
 const dotenv = require("dotenv")
 const cors = require("cors")
 const sequelize = require("./db")
-const projectRouter = require("./routes/projects")
+const studenstRouter = require("./routes/students") 
+const projectsRouter = require("./routes/projects")
+const reviewsRouter = require("./routes/reviews")
+
 
 // to check out if it is working. Promisse
 sequelize.authenticate()
          .then(() => console.log("It is working"))
          .catch((e)=>console.log(e))
 
+
 const server = express()
 server.use(cors())
 server.use(express.json())
 
+
 //routes:
-server.use("/projects", projectRouter)
+server.use("/projects", projectsRouter)
+server.use("/students", studenstRouter)
+server.use("/reviews", reviewsRouter)
 
 
 
