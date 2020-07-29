bb0785683cffeea65b973fb568292b96075f3f3b
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-29 21:12:40 +0200

    Update App.js

--- Modul-6/Week-15/m5-w15-d4/frontend/src/App.js
+++ Modul-6/Week-15/m5-w15-d4/frontend/src/App.js
@@ -1,54 +1,29 @@
 import React, { Component } from 'react'
 import StudentList from './components/StudentList';
-import ProjectList from "./components/ProjectList"
 
 
 export default class App extends Component {
   state = {
-    students: [],
-    projects: []
+    students: []
   }
-
   render() {
     return (
       <div>
         <h1> HEllo world!</h1>
         <StudentList students={this.state.students} />
-        <ProjectList projects={this.state.projects} />
       </div>
     )
   }
   
-  // fetchProjects = async()=>{
-  //   const res = await fetch("http://localhost:4000/projects")
-  //   //http://localhost:3032/students
-  //   //http:&//localhost:5432/students
-  //   const projects = await res.json()
-  //   this.setState({
-  //     projects: projects.rows
-  //   })
-
-  // }
-
-  // fetchStudents = async() => {
-  //   const res = await fetch("http://localhost:4000/projects")
-  //   //http://localhost:3032/students
-  //   //http:&//localhost:5432/students
-  //   const students = await res.json()
-  //   this.setState({
-  //     students: students.rows
-  //   })
-  // }
+  
 
   componentDidMount = async ()=>{
-    const res = await fetch("http://localhost:4000/projects")
+    const res = await fetch("http://localhost:4000/students")
     //http://localhost:3032/students
     //http:&//localhost:5432/students
-    const projects = await res.json()
+    const students = await res.json()
     this.setState({
-      projects: projects.rows
+      students: students
     })
-    
   }
 }
-
