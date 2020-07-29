85f11582673494d0956a038f8d84838d03eba9e3
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-29 21:12:05 +0200

    Update index.js

--- Modul-6/Week-15/m5-w15-d4/backend/src/model/project/index.js
+++ Modul-6/Week-15/m5-w15-d4/backend/src/model/project/index.js
@@ -1,35 +1,41 @@
-const orm = require("../../db")
-const sequelize = require("sequelize")
-const { Sequelize } = require("sequelize")
+const orm = require("../../db");
+const Sequelize = require("sequelize");
 
-// .define is the command to create the new schema
-const Project = orm.define("Projects",{
-  projectId: {
-    type: Sequelize.NUMBER,
-    primaryKey: true
+const Project = orm.define(
+  "Projects",
+  {
+    projectId: {
+      type: Sequelize.NUMBER,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    repoUrl: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    liveUrl: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    creationDate: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    studentId: {
+      type: Sequelize.NUMBER,
+      allowNull: false,
+    },
   },
-  repoUrl: {
-    type: Sequelize.STRING,
-    allowNull: true 
-  },
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false 
-  },
-  liveUrl: {
-    type: Sequelize.STRING,
-    allowNull: true 
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false 
-  },
-  creationDate: {
-    type: Sequelize.STRING,
-    allowNull: false 
+  {
+    timestamps: false,
   }
- },{
-    timestamps: false  
-  })
+);
 
-module.exports = Project
+module.exports = Project;
